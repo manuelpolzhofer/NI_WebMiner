c68789919b224a1b27e4189ac5723ac1ff1ab826
Manuel Polzhofer <manuel.polzhofer@gmail.com>
2014-12-02 00:49:55 +0100

    stemmer added.

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,9 +1,13 @@
 package main;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 import pagerank.PageRank;
+import textminer.Document;
+import textminer.Preprocesser;
 import crawler.NICrawler;
 import crawler.Website;
 import crawler.WebsiteWriter;
@@ -16,7 +20,7 @@ public class Main {
     	NICrawler niCrawler = new NICrawler();
     	String startUrl = "http://en.wikipedia.org/wiki/Data_mining";
     	//String startUrl = "http://derstandard.at";
-    	int maxNumberOfVisit = 100;
+    	int maxNumberOfVisit = 5;
     	niCrawler.startCrawling(startUrl,maxNumberOfVisit);
     	
     	HashMap<String,Website> visitedWebsites = niCrawler.getVisitedWebsites();
@@ -30,6 +34,36 @@ public class Main {
     	PageRank pageRanke = new PageRank(websites);
     	pageRanke.calculatePagranks();
     	
+    	
+		ArrayList<Document> docList;
+		
+		
+		String stopwords = "../stopwords.txt";
+		
+		
+		
+		File folderFile = new File("korpus");
+		File[] fileList = folderFile.listFiles();
+		
+		// Preprocessing
+		File dir = null;
+		try {
+			dir = Preprocesser.applyStemmer((Preprocesser.removeStopWords(fileList, stopwords)).listFiles());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Throwable e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		//String query = new String("")
+		
+		//Generate TF-IDF vector representation
+		Document d = new Document(null,null);		
+		docList = d.processAllDocuments(dir);
+		
     	
     
     	
